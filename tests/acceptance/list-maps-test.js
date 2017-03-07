9f1a3ef1837ce982c61200d5de3e2bb8dd8359bd
woosungchu <woosungchu@gmail.com>
2017-03-07 13:36:48 +0900

    back to write testcase and merge mirage branch

---
+++ tests/acceptance/list-maps-test.js
@@ -0,0 +1,40 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'mindmap-web/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list maps');
+
+test('should redirect to map list', function(assert){
+	
+});
+
+test('should list up-to-date popular maps', function(assert){
+	
+});
+
+test('should list user\'s maps order by up-to-date', function(assert){
+	
+});
+
+test('should link to map editor', function(assert){
+	
+});
+
+test('should link to detail view for a specific map', function(assert){
+	
+});
+
+test('should link to sign up and login for an authorized user', function(assert){
+	
+});
+
+test('should link to map list', function(assert){
+	
+});
+
+//test('visiting /list-maps', function(assert) {
+//  visit('/list-maps');
+//
+//  andThen(function() {
+//    assert.equal(currentURL(), '/list-maps');
+//  });
+//});
